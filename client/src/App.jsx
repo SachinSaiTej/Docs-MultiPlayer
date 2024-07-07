83dfb4dd17b1bfa22f588a08f940b33359cb6a76
SachinSaiTej <saitejakm2001@gmail.com>
2024-07-07 23:14:27 +0530

    Added the Quill editor into the app.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import TextEditor from "./components/TextEditor";
+
+function App() {
+  return (
+    <>
+      <h1>Google-Docs Clone</h1>
+      <TextEditor />
+    </>
+  );
+}
+
+export default App;
